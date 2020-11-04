901a183d49777a4deba0422cce5f72de34560c99
Kirill Semenov <kirikx@mail.ru>
2020-11-04 14:47:40 +0300

    Lesson2 Done

---
+++ home-works/src/main/webapp/static/product.jsp
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Product</title>
+</head>
+<body>
+<form action="order" method="get">
+    <label for="name">Name</label>
+    <input type="text" title="Name" content="Name" id="name">
+    <label for="price">Price</label>
+    <input type="number" title="Price" content="Price" id="price">
+    <label for="description">Description</label>
+    <input type="text" title="Description" content="Description" id="description">
+    <button type="submit">Save</button>
+</form>
+</body>
+</html>
--- jee-servlet/src/main/java/ru/kirikomp/FirstServlet.java
+++ jee-servlet/src/main/java/ru/kirikomp/FirstServlet.java
@@ -18,8 +18,10 @@ public class FirstServlet implements Servlet {
     }
 
     @Override
-    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
-        servletResponse.getWriter().println("<h1>Hello, First Servlet!!</h1>");
+    public void service(ServletRequest req, ServletResponse resp) throws ServletException, IOException {
+        getServletConfig().getServletContext().getRequestDispatcher("/page_header").include(req, resp);
+
+        resp.getWriter().println("<h1>Hello, First Servlet!!</h1>");
     }
 
     @Override
